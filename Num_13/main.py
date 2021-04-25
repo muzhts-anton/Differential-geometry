0fee02ba979b6d002c4c8016843573604c4bbc19
Liza <54031870+Liza-hub@users.noreply.github.com>
2021-04-25 17:41:43 +0300

    Create main.py

---
+++ Num_13/main.py
@@ -0,0 +1,15 @@
+# T**B = T^ij * Bji
+from sympy import *
+T = Matrix([
+    [1, 2],
+    [0, 7]
+])
+B = Matrix([
+    [3, -1],
+    [2, 1]
+])
+rez = 0
+for i in range(2):
+    for j in range(2):
+        rez += T[i, j] * B[j, i]
+rez
