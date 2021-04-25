ee89a7c81560bdccb0385c7c50464d9f6c495c02
Liza <54031870+Liza-hub@users.noreply.github.com>
2021-04-25 17:28:16 +0300

    Update main.py

--- Num_11/main.py
+++ Num_11/main.py
@@ -14,3 +14,66 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+from sympy import *
+T = Matrix([
+    [1, 0],
+    [6, 2]
+])
+B = Matrix([
+    [8, 1],
+    [3, 7]
+])
+Q = Matrix([
+    [1, 0],
+    [1, 2]
+])
+
+# метрическая матрица g[i,j] = ei * ej
+g = Matrix([
+    [0, 0],
+    [0, 0]
+])
+for i in range(2):
+    for j in range(2):
+        for m in range(2):
+            g[i, j] += Q[m, i] * Q[m, j]
+
+P = Q**(-1)
+
+# вычилим контравариантные компоненты тензора С в декартовом ортонормированном базисе
+# C_^ik = T^ij * Bj^k
+C_ = Matrix([
+    [0, 0],
+    [0, 0]
+])
+for i in range(2):
+    for k in range(2):
+        for j in range(2):
+            C_[i, k] += T[i, j] * B[k, j]
+
+# вычислим контраваринатные компонены С в в базисе е
+# C^ml = C^ik * Pi^m * Pk^l
+C = Matrix([
+    [0, 0],
+    [0, 0]
+])
+for m in range(2):
+    for l in range(2):
+        for i in range(2):
+            for k in range(2):
+                C[m , l] += C_[i, k] * P[m, i] * P[l, k]
+
+# вычислим ковариантные компоненты С в базисе е
+Cst = C^ml * gms * glt 
+c = Matrix([
+    [0, 0],
+    [0, 0]
+])
+for s in range(2):
+    for t in range(2):
+        for m in range(2):
+            for l in range(2):
+                c[s, t] += C[m, l] * g[m, s] * g[l, t]
